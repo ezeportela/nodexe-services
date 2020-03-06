857cefdcdbdda05bc3862c6f5a3d1435cc90d275
Ezequiel Portela <eportelab@gmail.com>
2020-03-06 09:27:49 -0300

    fix configService

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const ConfigService = require('./src/services/configService');
+const configService = require('./src/services/configService');
 const TokenCacheService = require('./src/services/tokenCacheService');
 const tokenCacheRepository = require('./src/services/tokenCacheRepository');
 
-module.exports = {ConfigService, TokenCacheService, tokenCacheRepository};
+module.exports = {configService, TokenCacheService, tokenCacheRepository};
