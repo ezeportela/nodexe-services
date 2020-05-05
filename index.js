b9ac9c28c773ac9218aa5746c8dc61c04f063995
Ezequiel Portela <eportelab@gmail.com>
2020-05-05 13:27:58 -0300

    Add HttpService

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 const configService = require('./src/services/configService');
 const TokenCacheService = require('./src/services/tokenCacheService');
 const tokenCacheRepository = require('./src/services/tokenCacheRepository');
+const HttpService = require('./src/services/httpService');
 
-module.exports = {configService, TokenCacheService, tokenCacheRepository};
+module.exports = {
+  configService,
+  TokenCacheService,
+  tokenCacheRepository,
+  HttpService,
+};
---
+++ src/services/httpService.js
@@ -0,0 +1,65 @@
+const axios = require('axios');
+const url = require('url');
+const _ = require('lodash');
+
+class HttpService {
+  constructor({name, method, logger}) {
+    this.name = name;
+    this.method = method;
+    this.bodyType = method === 'get' ? 'params' : 'data';
+    this.logger = logger;
+  }
+
+  getConnection() {
+    throw new Error('the connection has not implemented yet.');
+  }
+
+  getURI(requestData = {}) {
+    let uri = url.format(this.getConnection());
+    for (const key of Object.keys(requestData)) {
+      uri = uri.replace(`:${key}`, requestData[key]);
+    }
+    return uri;
+  }
+
+  async getHeaders(requestData) {
+    return null;
+  }
+
+  getRequestData(requestData) {
+    return requestData;
+  }
+
+  getSSL() {
+    return {};
+  }
+
+  async request(requestData) {
+    const options = {
+      method: this.method,
+      url: this.getURI(requestData),
+      headers: await this.getHeaders(requestData),
+      [this.bodyType]: this.getRequestData(requestData),
+    };
+
+    this.logger('start_call_service', options);
+
+    const ssl = this.getSSL();
+
+    if (!_.isEmpty(ssl)) {
+      Object.assign(options, {...ssl});
+    }
+
+    const response = await axios(options);
+
+    this.logger('end_call_service', {
+      name: this.name,
+      headers: await this.getHeaders(requestData),
+      response: response.data,
+    });
+
+    return response;
+  }
+}
+
+module.exports = HttpService;
